2da470ade317f63e32493b857707b61a8ce406d2
Shaunakdas <shaunak.das@tokopedia.com>
2019-08-16 19:27:51 +0530

    Updates

---
+++ precache-manifest.ddcf2f31435cc59a0209af5178eb828a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30738cc1b005cd7d93f80099749272b",
+    "url": "/drona-dashboard/index.html"
+  },
+  {
+    "revision": "ad3da28b0a45b4f48c4b",
+    "url": "/drona-dashboard/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5a1cf258492b4bc0c82f",
+    "url": "/drona-dashboard/static/js/2.c54e7202.chunk.js"
+  },
+  {
+    "revision": "ad3da28b0a45b4f48c4b",
+    "url": "/drona-dashboard/static/js/main.599241e2.chunk.js"
+  },
+  {
+    "revision": "9725a7102e4d62614de1",
+    "url": "/drona-dashboard/static/js/runtime~main.4df67040.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drona-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
